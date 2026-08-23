12242d2df08bb835d6a27737dcd0ae3eac6811d7
eslam21006-coding <eslam21006@gmail.com>
2026-08-23 17:30:45 +0300

    Share ZIP CRC test helper

---
+++ test/helpers/zip-crc.ts
@@ -0,0 +1,15 @@
+const CRC32_TABLE = Array.from({ length: 256 }, (_, index) => {
+  let value = index;
+  for (let bit = 0; bit < 8; bit += 1) {
+    value = (value & 1) !== 0 ? 0xedb88320 ^ (value >>> 1) : value >>> 1;
+  }
+  return value >>> 0;
+});
+
+export function crc32Checksum(bytes: Uint8Array) {
+  let crc = 0xffffffff;
+  for (let index = 0; index < bytes.length; index += 1) {
+    crc = CRC32_TABLE[(crc ^ bytes[index]) & 0xff] ^ (crc >>> 8);
+  }
+  return (crc ^ 0xffffffff) >>> 0;
+}
